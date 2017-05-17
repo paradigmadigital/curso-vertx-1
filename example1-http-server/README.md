0cb737c695c1eb2ec14ac3ca13a7cc09dac47f34
J Manuel Garc;ia Rozas <jmgrozas@paradigmatecnologico.com>
2017-05-17 21:59:53 +0200

    cambios

--- example1-http-server/README.md
+++ example1-http-server/README.md
@@ -9,7 +9,7 @@ La finalidad será crear:
 
  - Seleccionar la clase Starter
  - Añadir el comando run PAQUETERIA_Y_NOMBRE_DE_VERTICLE -conf ruta_al_fichero_de_configuracion
- - Añadir la variable de entorno de java  -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory
+ - Añadir la variable de entorno de java  -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dhazelcast.logging.type=slf4j
  - Añadir la variable de entorno de java para el debug de -Dvertx.options.blockedThreadCheckInterval=1000000 
 
 # Build and run 
--- example2-templates-client/build_and_run.sh
+++ example2-templates-client/build_and_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-JMX_OPTIONS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=7778 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=192.168.32.103 "
+JMX_OPTIONS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=7778 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=paradigma.cursovertx "
 mvn clean package -DskipTests 
 java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/example2-templates-client-fat.jar -conf target/classes/config.json  
