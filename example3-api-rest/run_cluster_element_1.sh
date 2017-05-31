581de45c802ccda06b6f9ee2e784961aec6f0ad3
J Manuel Garc;ia Rozas <jmgrozas@paradigmatecnologico.com>
2017-05-31 16:58:00 +0200

    añadido soporte para jmx

--- example3-api-rest/run_cluster_element_1.sh
+++ example3-api-rest/run_cluster_element_1.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/example3-api-rest-fat.jar -cluster goku -conf src/config/config.json
+JMX_OPTIONS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=7778 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=paradigma.cursovertx "
+java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/example3-api-rest-fat.jar -cluster goku -conf src/config/config.json
